0f84819e58a87b3c6362f033cecad6aada50c138
ayush2k <ayush2k@gmail.com>
2016-10-27 02:45:36 -0700

    removes extraneous dependency

--- src/main/java/securbank/config/WebSecurityConfig.java
+++ src/main/java/securbank/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package securbank.config;
 
-import org.aspectj.weaver.ast.And;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
